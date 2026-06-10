121c905efa56879899339e1dfee7577a764f8e0d
MDshobana <shobanaa10@gmail.com>
2026-06-11 00:16:59 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.set("trust proxy", 1);
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://capstone-v2-indol.vercel.app"],
+        "https://mylearningportal.site"],
 
     credentials: true
 }));
