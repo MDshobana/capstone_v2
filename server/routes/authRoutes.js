3484070c07421043754bad17372b487bbea0ea85
MDshobana <shobanaa10@gmail.com>
2026-06-10 23:45:11 +0200

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -221,7 +221,6 @@ router.post('/google-login', async (req, res) => {
             secure: true, // process.env.NODE_ENV === 'production',
             sameSite: "none",
             path: "/",
-            domain: ".onrender.com",
             maxAge: 24 * 60 * 60 * 1000
         })
 
@@ -327,7 +326,6 @@ router.post('/login', async (req, res) => {
             secure: true, // process.env.NODE_ENV === 'production',
             sameSite: "none",
             path: "/",
-            domain: ".onrender.com",
             maxAge: 60 * 60 * 1000
 
         })
