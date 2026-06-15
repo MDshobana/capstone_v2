b30aaa0fc43ba248afd097e09d26570d971046e1
MDshobana <shobanaa10@gmail.com>
2026-06-15 12:19:52 +0200

    Update setupTests.js

--- server/tests/setupTests.js
+++ server/tests/setupTests.js
@@ -50,3 +50,20 @@ vi.mock("razorpay", () => ({
     }
   }
 }));
+
+vi.mock("../middleware/auth.js", () => ({
+  default: (req, res, next) => {
+    req.user = {
+
+    id: "507f1f77bcf86cd799439011",
+    role: "student"
+
+  };
+  next();
+}
+}));
+
+
+vi.mock("../middleware/authorize.js", () => ({
+  default: () => (req, res, next) => next()
+}));
