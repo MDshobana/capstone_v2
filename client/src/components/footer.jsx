f32f652606571e511490a254bd186f301cd9d16b
MDshobana <shobanaa10@gmail.com>
2026-06-18 11:57:08 +0200

    Add files via upload

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Footer() {
+    return (
+        <footer className="text-center py-6 text-gray-500">
+            © 2026 MyLearning Portal
+        </footer>
+    );
+}
